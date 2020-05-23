b67c83eb3eb805fc7899555492973aea97741820
kamitory <neco.hoosen@gmail.com>
2020-05-24 05:52:08 +0900

    - 유니티버전 : Unity 2019.3.14f1 (64-bit)
    1.게임오브젝트
    2.트랜스폼
    3.리지드 바디
    4.컬라이더
    5.메쉬 렌더러
    6.카메라
    7.라이트
    8.애니메이션
    9.애니메이터

--- Unity_Toturial/Assets/Scripts/Test.cs
+++ Unity_Toturial/Assets/Scripts/Test.cs
@@ -4,27 +4,93 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    private Animation anim;
+    private Animator anim;
+    private Rigidbody rigid;
+    private BoxCollider col;
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private LayerMask layerMask;
+
+
+    private bool isMove;
+    private bool isJump;
+    private bool isFall;
 
 
     void Start()
     {
-        anim = GetComponent<Animation>(); 
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+        col = GetComponent<BoxCollider>();
     }
     
    void Update()
     {
-       if (Input.GetKeyDown(KeyCode.W))
+        TryWalk();
+        TryJump();
+     
+    }
+
+    private void TryJump()
+    {
+        if (isJump)
         {
-           // anim.Play("New_Animation2");
-           // anim.PlayQueued("New_Animation2");
-           anim.Blend("New_Animation2");
-           // anim.CrossFade("New_Animation2");
-           // if (anim.IsPlaying("New_Animation2"))
-           //     anim.Play("New_Animation2");
+            if(rigid.velocity.y <= -0.1f && !isFall)
+            {
+                isFall = true;
+                anim.SetTrigger("Fall");
+            }
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(transform.position, -transform.up, out hitInfo, col.bounds.extents.y+ 0.1f, layerMask ))
+            {
+                anim.SetTrigger("Land");
+                isJump = false;
+                isFall = false;
+            }
         }
+
+        if(Input.GetKeyDown(KeyCode.Space) && !isJump)
+        {
+            isJump = true;
+            rigid.AddForce(Vector3.up * jumpForce);
+            anim.SetTrigger("Jump");
+        }
+    }
+    private void TryWalk()
+    {
+        float _dirX = Input.GetAxisRaw("Horizontal");
+        float _dirZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(_dirX, 0, _dirZ);
+
+        //anim.SetBool("Right", false);
+        //anim.SetBool("Left", false);
+        //anim.SetBool("Up", false);
+        //anim.SetBool("Down", false);
+        isMove = false;
+        if (direction != Vector3.zero)
+        {
+            isMove = true;
+            this.transform.Translate(direction.normalized * moveSpeed * Time.deltaTime);
+
+            //if (direction.x > 0)
+            //    anim.SetBool("Right", true);
+            //else if(direction.x < 0)
+            //    anim.SetBool("Left", true);
+            //else if (direction.z > 0)
+            //    anim.SetBool("Up", true);
+            //else if (direction.z < 0)
+            //    anim.SetBool("Down", true);
+        }
+
+        anim.SetBool("Move", isMove);
+        anim.SetFloat("DirX", direction.x);
+        anim.SetFloat("DirZ", direction.z);
     }
 
 
-    
 }
