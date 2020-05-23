39cd6d7349a8cdd5656e7a5422ada72296b42de8
kamitory <63181292+kamitory@users.noreply.github.com>
2020-05-23 23:53:58 +0900

    - 유니티버전 : Unity 2019.3.14f1 (64-bit)
    1.게임오브젝트
    2.트랜스폼
    3.리지드 바디
    4.컬라이더
    5.메쉬 렌더러

--- Unity_Toturial/Assets/Scripts/Test.cs
+++ Unity_Toturial/Assets/Scripts/Test.cs
@@ -5,51 +5,31 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     [SerializeField]
-    private BoxCollider col;
-    
+    private Material violet_Mat;
+    [SerializeField]
+    private Material bluegreen_Mat;
+
+    private MeshRenderer mesh;
 
     //Vector3 rotation;
     void Start()
     {
-        col = GetComponent<BoxCollider>();
+        mesh = GetComponent<MeshRenderer>();
     }
-
-    // Update is called once per frame
-    //void Update()
-    //{
-    //   //if (Input.GetKeyDown(KeyCode.W))
-    //   //{
-    //   //    Debug.Log("col.bounds " + col.bounds);
-    //   //    Debug.Log("col.bounds.extents " + col.bounds.extents);
-    //   //    Debug.Log("col.bounds.extents.x " + col.bounds.extents.x);
-    //   //    Debug.Log("col.size " + col.size);
-    //   //    Debug.Log("col.center " + col.center);
-    //   //
-    //   //}
-    //
-    //    if ( Input.GetMouseButtonDown(0))
-    //    {
-    //        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-    //        RaycastHit hitInfo;
-    //       if( col.Raycast(ray, out hitInfo, 1000))
-    //        {
-    //            this.transform.position = hitInfo.point;
-    //        }
-    //    }
-    //}
-
-
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    
-    //}
-    private void OnTriggerStay(Collider other)
+    
+   void Update()
     {
-        other.transform.position += new Vector3(0, 0, 0.1f);
+        if(Input.GetMouseButton(0))
+        {
+            mesh.material = bluegreen_Mat;
+        }
+        else
+        {
+            mesh.material = violet_Mat;
+
+        }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        other.transform.position = new Vector3(0, 2, 0);
-    }
+
+    
 }
