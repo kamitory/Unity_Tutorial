7b7680ac2fbada1630078ec8a9706d61f6d2d2b7
kamitory <neco.hoosen@gmail.com>
2020-05-24 04:10:27 +0900

    - 유니티버전 : Unity 2019.3.14f1 (64-bit)
    1.게임오브젝트
    2.트랜스폼
    3.리지드 바디
    4.컬라이더
    5.메쉬 렌더러
    6.카메라
    7.라이트
    8.애니메이션

--- Unity_Toturial/Assets/Scripts/Test.cs
+++ Unity_Toturial/Assets/Scripts/Test.cs
@@ -4,33 +4,24 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    private Light theLight;
+    private Animation anim;
 
-    private float targetIntensity;
-    private float currentIntensity;
 
     void Start()
     {
-        theLight = GetComponent<Light>();
-        currentIntensity = theLight.intensity;
-        targetIntensity = Random.Range(0.4f, 1f);
+        anim = GetComponent<Animation>(); 
     }
     
    void Update()
     {
-        if(Mathf.Abs(targetIntensity - currentIntensity) >= 0.01)
+       if (Input.GetKeyDown(KeyCode.W))
         {
-            if (targetIntensity - currentIntensity >= 0)
-                currentIntensity += Time.deltaTime * 3f;
-            else
-                currentIntensity -= Time.deltaTime * 3f;
-
-            theLight.intensity = currentIntensity;
-            theLight.range = currentIntensity + 10;
-        }
-        else
-        {
-            targetIntensity = Random.Range(0.4f, 1f);
+           // anim.Play("New_Animation2");
+           // anim.PlayQueued("New_Animation2");
+           anim.Blend("New_Animation2");
+           // anim.CrossFade("New_Animation2");
+           // if (anim.IsPlaying("New_Animation2"))
+           //     anim.Play("New_Animation2");
         }
     }
 
