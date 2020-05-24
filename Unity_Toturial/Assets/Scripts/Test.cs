ef1f2e5ff81f6a9ac89508c494dcde2e7b5da427
kamitory <neco.hoosen@gmail.com>
2020-05-24 17:49:56 +0900

    - 유니티버전 : Unity 2019.3.14f1 (64-bit)
    1.게임오브젝트
    2.트랜스폼
    3.리지드 바디
    4.컬라이더
    5.메쉬 렌더러
    6.카메라
    7.라이트
    8.애니메이션
    9.애니메이터
    10.파티클 시스템
    11.UI의 기본
    12.슬라이더UI
    13.인풋 필드 UI
    14.스크롤바 UI
    15.대화 시스템

--- Unity_Toturial/Assets/Scripts/Test.cs
+++ Unity_Toturial/Assets/Scripts/Test.cs
@@ -3,24 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class Dialogue
+{
+    [TextArea]
+    public string dialogue;
+    public Sprite cg;
+}
+
 public class Test : MonoBehaviour
 {
-    [SerializeField] private Text txt_Money;
-    [SerializeField] private InputField inputTxT_Money;
+    [SerializeField] private SpriteRenderer sprite_StandingCG;
+    [SerializeField] private SpriteRenderer sprite_DialogueBox;
+    [SerializeField] private Text txt_Dialogue;
 
-    private int currentMoney;
+    private bool isDialogue = false;
 
-    public void input()
+    private int count = 0;
+
+    [SerializeField] private Dialogue[] dialogue;
+
+    public void ShowDialogue()
     {
-        currentMoney += int.Parse(inputTxT_Money.text);
-
-        txt_Money.text = currentMoney.ToString();
+        OnOff(true);
+       
+        count = 0;
+        NextDialogue(); 
     }
-    public void output()
+    private void OnOff(bool _flag)
     {
-        currentMoney -= int.Parse(inputTxT_Money.text);
+        sprite_DialogueBox.gameObject.SetActive(_flag);
+        sprite_StandingCG.gameObject.SetActive(_flag);
+        txt_Dialogue.gameObject.SetActive(_flag);
+        isDialogue = _flag;
+    }
+    //private void HideDialogue()
+    //{
+    //    sprite_DialogueBox.gameObject.SetActive(false);
+    //    sprite_StandingCG.gameObject.SetActive(false);
+    //    txt_Dialogue.gameObject.SetActive(false);
+    //    isDialogue = false;
+    //}
 
-        txt_Money.text = currentMoney.ToString();
+    private void NextDialogue()
+    {
+        txt_Dialogue.text = dialogue[count].dialogue;
+        sprite_StandingCG.sprite = dialogue[count].cg;
+        count++;
+    }
+
+    private void Update()
+    {
+        if(isDialogue)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                if (count < dialogue.Length)
+                    NextDialogue();
+                else
+                    OnOff(false);
+            }
+        }
     }
 
 }
