4ccb0409442f10d1e32175e4c732c4ad8c0ff455
kamitory <neco.hoosen@gmail.com>
2020-05-24 19:37:09 +0900

    - 유니티버전 : Unity 2019.3.14f1 (64-bit)
    1.게임오브젝트
    2.트랜스폼
    3.리지드 바디
    4.컬라이더
    5.메쉬 렌더러
    6.카메라
    7.라이트
    8.애니메이션
    9.애니메이터
    10.파티클 시스템
    11.UI의 기본
    12.슬라이더UI
    13.인풋 필드 UI
    14.스크롤바 UI
    15.대화 시스템
    16.오디오 소스
    17.Physics 클래스

---
+++ Unity_Toturial/Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+    [SerializeField] private float damage;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.transform.name + "에게 데미지 " + damage + "을 입혔습니다.");
+        Destroy(this.gameObject);
+    }
+}
--- Unity_Toturial/Assets/Scripts/Test.cs
+++ Unity_Toturial/Assets/Scripts/Test.cs
@@ -13,23 +13,59 @@ public class Dialogue
 
 public class Test : MonoBehaviour
 {
-    private AudioSource theAudio;
+   // private AudioSource theAudio;
+   //
+   // [SerializeField] private LayerMask layerMask;
+    [SerializeField] private GameObject go_BulletPrefab;
+    private float createTime = 1f;
+    private float currentCreateTime = 0;
+   //
+   //
+   //
+   // void Start()
+   // {
+   // }
 
-    [SerializeField] private AudioClip[] clip;
-
-
-
-    void Start()
+    void Update()
     {
-        theAudio = GetComponent<AudioSource>();
+      currentCreateTime += Time.deltaTime;
+
+
+
+        Collider[] col = Physics.OverlapSphere(transform.position, 5f);
+
+        if(col.Length >0)
+        {
+            for (int i = 0; i < col.Length; i++)
+            {
+                Transform tf_Target = col[i].transform;
+                if (tf_Target.tag == "Player")
+                {
+                    Quaternion rotation = Quaternion.LookRotation(tf_Target.position - this.transform.position);
+                    transform.rotation = rotation;
+
+                    if (currentCreateTime >= createTime)
+                    {
+                        //    currentCreateTime = 0f;
+                        //    RaycastHit hitInfo;
+                        //    if (Physics.Raycast(this.transform.position, this.transform.forward, out hitInfo, 10f , layerMask))
+                        //    {
+                        //        if (hitInfo.transform.tag == "Player")
+                        //        {
+                        Instantiate(go_BulletPrefab, transform.position, rotation);
+                        currentCreateTime = 0;
+
+
+                        //GameObject _temp = Instantiate(go_BulletPrefab, transform.position, rotation);
+                        //Physics.IgnoreCollision(_temp.GetComponent<Collider>(), tf_Target.GetComponent<Collider>());
+                        //currentCreateTime = 0;
+                        //        }
+                        //    }
+                    }
+                }
+            }
+        }
     }
 
-    public void PlaySE()
-    {
-        int _temp = Random.Range(0, 4);
-
-        theAudio.clip = clip[_temp];
-        theAudio.Play();
-    }
 
 }
