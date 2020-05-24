50c31d5aea94b3488efc7272b761cc2fefdb12d1
kamitory <neco.hoosen@gmail.com>
2020-05-25 01:46:50 +0900

    - 유니티버전 : Unity 2019.3.14f1 (64-bit)
    1.게임오브젝트
    2.트랜스폼
    3.리지드 바디
    4.컬라이더
    5.메쉬 렌더러
    6.카메라
    7.라이트
    8.애니메이션
    9.애니메이터
    10.파티클 시스템
    11.UI의 기본
    12.슬라이더UI
    13.인풋 필드 UI
    14.스크롤바 UI
    15.대화 시스템
    16.오디오 소스
    17.Physics 클래스
    18.텍스쳐와 메테리얼
    19.지형제작
    20.네비게이션
    21.경로의 거리 계산
    22.스카이 박스
    23.드래그&드롭

--- Unity_Toturial/Assets/Scripts/Test.cs
+++ Unity_Toturial/Assets/Scripts/Test.cs
@@ -1,62 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+using UnityEngine.EventSystems;
 
 
-public class Test : MonoBehaviour
+public class Test : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
 {
-    //Rigidbody myRigid;
-    //[SerializeField] private float moveSpeed;
-
-    NavMeshAgent agent;
-
-   // [SerializeField] private Transform[] tf_Destination;
-   // private Vector3[] wayPoint;
-
-    private void Start()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        //myRigid = GetComponent<Rigidbody>();
-        agent = GetComponent<NavMeshAgent>();
+        //Debug.Log("Begin Drag");
+        transform.position = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Drag");
         
     }
 
-    private void Update()
+    public void OnDrop(PointerEventData eventData)
     {
-        Collider[] col = Physics.OverlapSphere(transform.position, 10f);
-
-        if(col.Length >0)
-        {
-            for(int i = 0; i < col.Length; i++)
-            {
-                Transform tf_Target = col[i].transform;
-
-                if(tf_Target.name == "Player")
-                {
-                    NavMeshPath path = new NavMeshPath();
-                    agent.CalculatePath(tf_Target.position, path);
-
-                    Vector3[] wayPoints = new Vector3[path.corners.Length + 2];
-                    wayPoints[0] = transform.position;
-                    wayPoints[wayPoints.Length - 1] = tf_Target.position;
-
-                    float _distance = 0f;
-                    for (int p = 0; p < path.corners.Length; p++)
-                    {
-                        wayPoints[p + 1] = path.corners[p];
-                        _distance += Vector3.Distance(wayPoints[p], wayPoints[p + 1]);
-                    }
-
-                    if(_distance <= 10f)
-                    {
-                        agent.SetDestination(tf_Target.position);
-                    }
-
-                }
-            }
-        }
-
+        Debug.Log("Drop");
+       
     }
 
-  
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End Drag");
+       
+    }
 }
