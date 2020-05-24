d8730223a20e3bb95c64f7abcb075689d4ad5de3
kamitory <neco.hoosen@gmail.com>
2020-05-24 14:00:36 +0900

    - 유니티버전 : Unity 2019.3.14f1 (64-bit)
    1.게임오브젝트
    2.트랜스폼
    3.리지드 바디
    4.컬라이더
    5.메쉬 렌더러
    6.카메라
    7.라이트
    8.애니메이션
    9.애니메이터
    10.파티클 시스템
    11.UI의 기본

--- Unity_Toturial/Assets/Scripts/Test.cs
+++ Unity_Toturial/Assets/Scripts/Test.cs
@@ -1,20 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
-    ParticleSystem ps;
+    [SerializeField] private Text txt_name;
+    [SerializeField] private Image image_name;
+    [SerializeField] private Sprite sprite;
 
-    void Start()
-    {
-      
-    }
+    private bool isCoolTime = false;
+
+    private float currentTime = 5f;
+    private float delayTime = 5f;
+    //[SerializeField] private UnityEngine.UI.Text txt_name;
     
-   void Update()
+    void Update()
     {
-        ps.Emit(100);
+        //image_name.sprite =
+        //image_name.color = Color.red; 
+
+        if (isCoolTime)
+        {
+            currentTime -= Time.deltaTime;
+            image_name.fillAmount = currentTime / delayTime;
+
+            if(currentTime <= 0)
+            {
+                isCoolTime = false;
+                currentTime = delayTime;
+                image_name.fillAmount = currentTime;
+            }
+        }
+    
     }
 
+
+    public void Change()
+    {
+        txt_name.text = "변경됨";
+        isCoolTime = true;
+        //image_name.fillAmount = 0.5f;
+    }
+
+    //void Start()
+    //{
+    //  
+    //}
 
 }
